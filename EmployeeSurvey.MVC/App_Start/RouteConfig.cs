eccac5f737e2e66e67c89cc8e610c5eb84998a12
d-karev <dobromir.karev@gmail.com>
2020-08-16 23:56:11 +0300

    Cleaned up the MVC project from the default stuff
    + configured the default route
    + added views and controllers

--- EmployeeSurvey.MVC/App_Start/RouteConfig.cs
+++ EmployeeSurvey.MVC/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace EmployeeSurvey.MVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Survey", action = "New", id = UrlParameter.Optional }
             );
         }
     }
---
+++ EmployeeSurvey.MVC/Controllers/SurveyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeSurvey.MVC.Controllers
+{
+    public class SurveyController : Controller
+    {
+        // GET: Survey
+        public ActionResult New()
+        {
+            return View();
+        }
+    }
+}
